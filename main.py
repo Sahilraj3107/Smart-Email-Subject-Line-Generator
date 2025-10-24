176b3a2e04fa9622ce972b7ef0474ab85bf4616b
Sahil Raj <sahilraj98.93.95@gmail.com>
2025-10-24 21:29:17 +0530

    Updated the main.py file

--- main.py
+++ main.py
@@ -13,19 +13,28 @@ load_dotenv()
 
 model = ChatGroq(model ="llama-3.1-8b-instant" , temperature=0.6)
 
-
-# response = model.invoke("What is the lifestyle of successful people?")
-# print(response.content)
-
 prompt_template = PromptTemplate.from_template("I will provide you the email body and you have to generate its title and subject, In professional way.And make it humanly as possible. The email body is: {email_body}")
 
-email = input("Enter you email body: ")
 
-prompt = prompt_template.format(email_body = email)
-response = model.invoke(prompt)
+def get_subject(email_body: str) -> str:
+    prompt = prompt_template.format(email_body = email_body)
+    response = model.invoke(prompt)
+    return response.content
 
+def main():
 
-print("The email title and subject is: ",response.content)
+    if os.getenv("CI"):
+        email = """Hi Team,I hope this message finds you well. I wanted to schedule a quick meeting to discuss the progress on our current project. We need to review the timeline and address any blockers that might be affecting our delivery.Could we meet this Thursday at 2 PM? Please let me know if this works for everyone, or suggest an alternative time that suits you better.Looking forward to hearing from you.Best regards,Sarah"""
 
+        print("Running in CI mode")
+
+    else:
+        print("Please enter the email body:")
+        email = input()
+        subject = get_subject(email)
+        print("Recommended Subject and Title are: ", subject)    
 # Hi Team,I hope this message finds you well. I wanted to schedule a quick meeting to discuss the progress on our current project. We need to review the timeline and address any blockers that might be affecting our delivery.Could we meet this Thursday at 2 PM? Please let me know if this works for everyone, or suggest an alternative time that suits you better.Looking forward to hearing from you.Best regards,Sarah
 
+
+if __name__ == "__main__":
+    main()
